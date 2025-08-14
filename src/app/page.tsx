66de0c028d4594be97ea4c1e7a658fed42d553c4
Long <longstyle2005@gmail.com>
2025-08-15 01:18:28 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 import styles from './page.module.scss'
-
+import Image from 'next/image';
 export default function HomePage({
     
 }: {
     
 }) {
     return (
-        <>
-            Top pGAe
-        </>
+        <div className={styles.wrap}>
+            <div className={styles.col1}>
+                <p className={styles.ttl}>Sign in</p>
+                
+                
+            </div>
+            <div className={styles.col2}>
+                <div className={styles.intro}>
+                    <Image 
+                        width={0}
+                        height={0}
+                        src='img/common/logo.svg'
+                        alt="icon"
+                        priority
+                        className={styles.introLogo}
+                    />
+                    <p className={styles.introTtl}>Welcome to Management</p>
+                </div>
+            </div>
+        </div>
     )
 }
