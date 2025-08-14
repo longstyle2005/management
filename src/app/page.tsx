0bede0a5fa8a7eccb1508dd3df637b8c5260036e
Long <longstyle2005@gmail.com>
2025-08-14 18:20:50 +0700

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import styles from './page.module.scss'
+
+export default function HomePage({
+    
+}: {
+    
+}) {
+    return (
+        <>
+            Top pGAe
+        </>
+    )
+}
