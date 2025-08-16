f7213c1372817f38c57cc80142a28c883441a64c
Long <longstyle2005@gmail.com>
2025-08-16 17:50:40 +0700

    update

--- src/app/component/SignIn/SignIn.tsx
+++ src/app/component/SignIn/SignIn.tsx
@@ -1,29 +1,18 @@
 'use client'
 
-import { useEffect, useRef, useState } from "react";
 import styles from './SignIn.module.scss'
 import { signIn, signOut, useSession } from "next-auth/react";
 import { usePathname } from 'next/navigation';
-import LoadingFullPage from '~/app/component/LoadingFullPage/LoadingFullPage';
 import { useSearchParams, useRouter } from "next/navigation";
-import { signInSchema } from "~/app/utils/validation/signInSchema";
-
+import { useForm } from "react-hook-form";
 import Link from "next/link";
+import { LoginForm } from '~/app/utils/interfaces/login_form.interface';
 
 export default function SignIn ( 
 	{ } : 
 	{ }
 ) {
 	const router = useRouter();
-	const searchParams = useSearchParams();
-	const errorMessage = searchParams ? searchParams.get("error") : null;
-	const popupRef = useRef<HTMLDivElement>(null);
-	const [isPopupAccount, setIsPopupAccount] = useState(false);
-    const [isLoadingFullPage, setIsLoadingFullPage] = useState(false);
-	const [error, setError] = useState<string | null>(null);
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-
 	const pathname = usePathname();
 
 	let isNotAdmin = null;
@@ -31,53 +20,56 @@ export default function SignIn (
 		isNotAdmin = !pathname.startsWith('/admin');
 	}
 
-	const handleAdminSignIn = async (e: React.FormEvent) => {
-		e.preventDefault();
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors },
+	} = useForm<LoginForm>();
+
+	const onSubmit = async (data: LoginForm) => {
 		const res = await signIn("credentials", {
-			email,
-			password,
-			redirect: true,
+			email: data.email,
+			password: data.email,
+			redirect: false,
 			callbackUrl: "/admin",
 		});
-	
 		if (res?.error) {
-			alert("Đăng nhập thất bại");
+			setError("root", { message: "Invalid email or password" });
+		} else {
+			router.push("/admin");
 		}
 	};
-	
-	useEffect(() => {
-		if (errorMessage) {
-			setError(errorMessage);
-			router.replace("/", undefined); 
-		}
-	}, [errorMessage, router]);
-
-	useEffect(() => {
-		const handleClickOutsidePopup = (e: MouseEvent) => {
-			if (popupRef.current && !popupRef.current.contains(e.target as Node)) {
-				setIsPopupAccount(false);
-			}
-		};
-		if (isPopupAccount) {
-		  	document.addEventListener('mousedown', handleClickOutsidePopup);
-		}
-		return () => {
-		  	document.removeEventListener('mousedown', handleClickOutsidePopup);
-		};
-	}, [isPopupAccount]);
 
   	return (
 		<div className={styles.blockLogin}>
-			<form className={styles.formBtn} onSubmit={handleAdminSignIn}>
+			<form className={styles.formBtn} onSubmit={handleSubmit(onSubmit)}>
 				<label className={`${styles.inputField} ${styles.email}`} htmlFor="email">
-					<input id="email" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+					<input
+						id="email" type="email" placeholder="Email"
+						// register: gắn input này vào react-hook-form + rule validate
+						{...register("email", { required: "Email is required" })}
+					/>
 				</label>
+				{errors.email && <p>{errors.email.message}</p>}
+
 				<label className={`${styles.inputField} ${styles.password}`} htmlFor="password">
-					<input id="password" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+					<input
+						id="password" placeholder="Password" type="password"
+						{...register("password", {
+							required: "Password is required",
+							minLength: { value: 6, message: "Min 6 characters" },
+						})}
+					/>
 					<span className={styles.eye}></span>
 				</label>
+				{errors.password && <p>{errors.password.message}</p>}
+
+
+				{errors.root && <p>{errors.root.message}</p>}
 				<button className={styles.btnLogin} type="submit">Sign in</button>
 			</form>
 		</div>
 	)
 }
+
